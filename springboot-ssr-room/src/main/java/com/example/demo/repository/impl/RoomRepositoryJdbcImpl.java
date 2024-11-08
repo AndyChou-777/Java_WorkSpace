cb666f910fce46f0c9c15d2e80cc308f32ce9156
NTPU <NTPU@503-11>
2024-11-08 16:01:12 +0800

    JavaWeb

--- springboot-ssr-room/src/main/java/com/example/demo/repository/impl/RoomRepositoryJdbcImpl.java
+++ springboot-ssr-room/src/main/java/com/example/demo/repository/impl/RoomRepositoryJdbcImpl.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -14,7 +16,9 @@ import com.example.demo.model.entity.Room;
 import com.example.demo.repository.RoomRepositoryJdbc;
 
 // 實現 RoomRepositoryJdbc 介面
-@Repository  //@Component
+//@Component = @Repository 只是此多用 Repository 方便認識
+@Repository("roomJdbc") // 括號為設定別名，方便其他程式中的方法引用，如沒設定則為預設類別名稱
+@PropertySource("classpath:sql.properties") // 自動到 src/main/resources 找到 sql.properties
 public class RoomRepositoryJdbcImpl implements RoomRepositoryJdbc {
 
 	private static final Logger logger = LoggerFactory.getLogger(RoomRepositoryJdbcImpl.class);
@@ -22,18 +26,33 @@ public class RoomRepositoryJdbcImpl implements RoomRepositoryJdbc {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
+	@Value("${room.sql.findAll}") // ${} SpringEL 語法
+	private String findAllSql;
+	
+	@Value("${room.sql.findById}")
+	private String findByIdSql;
+	
+	@Value("${room.sql.save}")
+	private String saveSql;
+	
+	@Value("${room.sql.update}")
+	private String updateSql;
+	
+	@Value("${room.sql.deleteById}")
+	private String deleteByIdSql;
+	
 	@Override
 	public List<Room> findAll() {
-		String sql = "select room_id, room_name, room_size from room";
-		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Room.class));
+		// String sql = "select room_id, room_name, room_size from room";
+		return jdbcTemplate.query(findAllSql, new BeanPropertyRowMapper<>(Room.class));
 	}
 
 	@Override
 	public Optional<Room> findById(Integer roomId) {
-		String sql = "select room_id, room_name, room_size from room where room_id = ?";
+		// String sql = "select room_id, room_name, room_size from room where room_id = ?";
 		// 因為 queryForObject 若沒有找到資料會自動拋出例外, 所以要 try-catch 保護
 		try {
-			Room room = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Room.class), roomId);
+			Room room = jdbcTemplate.queryForObject(findByIdSql, new BeanPropertyRowMapper<>(Room.class), roomId);
 			return Optional.of(room);
 		} catch (Exception e) {
 			logger.info(e.toString());
@@ -43,20 +62,20 @@ public class RoomRepositoryJdbcImpl implements RoomRepositoryJdbc {
 
 	@Override
 	public int save(Room room) {
-		String sql = "insert into room(room_id, room_name, room_size) values(?, ?, ?)";
-		return jdbcTemplate.update(sql, room.getRoomId(), room.getRoomName(), room.getRoomSize());
+		// String sql = "insert into room(room_id, room_name, room_size) values(?, ?, ?)";
+		return jdbcTemplate.update(saveSql, room.getRoomId(), room.getRoomName(), room.getRoomSize());
 	}
 
 	@Override
 	public int update(Room room) {
-		String sql = "update room set room_name = ?, room_size = ? where room_id = ?";
-		return jdbcTemplate.update(sql, room.getRoomName(), room.getRoomSize(), room.getRoomId());
+		// String sql = "update room set room_name = ?, room_size = ? where room_id = ?";
+		return jdbcTemplate.update(updateSql, room.getRoomName(), room.getRoomSize(), room.getRoomId());
 	}
 
 	@Override
 	public int deleteById(Integer roomId) {
-		String sql = "delete from room where room_id = ?";
-		return jdbcTemplate.update(sql, roomId);
+		// String sql = "delete from room where room_id = ?";
+		return jdbcTemplate.update(deleteByIdSql, roomId);
 	}
 
 }
--- springboot-ssr-room/src/test/java/com/example/demo/RoomJdbcTests.java
+++ springboot-ssr-room/src/test/java/com/example/demo/RoomJdbcTests.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.demo.model.entity.Room;
 import com.example.demo.repository.RoomRepositoryJdbc;
 
 @SpringBootTest
@@ -13,4 +15,36 @@ public class RoomJdbcTests {
 	@Qualifier("roomJdbc") // 因實作類別有別名，因此要輸入別名，方便查找要建立的實作類別
 	private RoomRepositoryJdbc roomRepositoryJdbc; // 如果沒有寫 @Qualifier 則會去抓他的實作類別，因此兩個以上實作要寫
 
+	@Test
+	public void testRoomAdd() {
+		Room room = new Room(101, "101(S)", 3);
+		int rowcount = roomRepositoryJdbc.save(room);
+		System.out.println("測試新增: " + room + " 結果回傳: " + rowcount);
+	}
+	
+	@Test
+	public void testFindAllRooms() {
+		System.out.println("測試查詢全部: " + roomRepositoryJdbc.findAll());
+	}
+	
+	@Test
+	public void testGetOneRoom() {
+		System.out.println("測試查詢單筆: " + roomRepositoryJdbc.findById(101));
+		System.out.println("測試查詢單筆: " + roomRepositoryJdbc.findById(109));
+	}
+	
+	@Test 
+	void updateRoom() {
+		Room uptRoom = new Room(101, "101(L)", 100);
+		int rowcount = roomRepositoryJdbc.update(uptRoom);
+		System.out.println("測試修改: " + uptRoom + " 結果回傳: " + rowcount + " (1 表示正確修改一筆)");
+	}
+	
+	@Test 
+	void deleteRoom() {
+		int roomId = 101;
+		int rowcount = roomRepositoryJdbc.deleteById(roomId);
+		System.out.println("測試刪除: " + roomId + " 結果回傳: " + rowcount + " (1 表示刪除修改一筆)");
+	}
+	
 }
