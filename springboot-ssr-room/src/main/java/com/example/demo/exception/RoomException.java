072f4e517be433f1f498f9074873f9f7324af84b
NTPU <NTPU@503-11>
2024-11-11 10:25:47 +0800

    Javaweb

---
+++ springboot-ssr-room/src/main/java/com/example/demo/exception/RoomException.java
@@ -0,0 +1,11 @@
+package com.example.demo.exception;
+
+import com.example.demo.model.entity.Room;
+
+public class RoomException extends RuntimeException{
+	
+	public RoomException(String message) {
+		super(message);
+	}
+
+}
