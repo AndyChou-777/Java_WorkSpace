0bbb859c016ce54b94f4b99132490b86cf4d72e4
NTPU <NTPU@503-11>
2024-11-11 14:50:10 +0800

    JavaWeb

--- springboot-ssr-room/src/main/java/com/example/demo/controller/RoomController.java
+++ springboot-ssr-room/src/main/java/com/example/demo/controller/RoomController.java
@@ -2,14 +2,23 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.exception.RoomException;
 import com.example.demo.model.dto.RoomDto;
 import com.example.demo.service.RoomService;
+
+import jakarta.validation.Valid;
 
 /**
  * Method URI            功能
@@ -17,9 +26,8 @@ import com.example.demo.service.RoomService;
  * GET    /rooms         查詢所有會議室(多筆)
  * GET    /room/{roomId} 查詢指定會議室(單筆)
  * POST   /room          新增會議室
- * PUT    /room/{roomId} 完整修改會議室(同時修改 roomName 與 roomSize)
- * PATCH  /room/{roomId} 部分欄位修改會議室(只修改  roomName 或 roomSize etc…)
- * DETETE /room/{roomId} 刪除會議室
+ * POST   /room/{roomId} 完整修改會議室(同時修改 roomName 與 roomSize)
+ * GET 	  /room/{roomId} 刪除會議室
  * --------------------------------------------------------------------
  * */
 
@@ -31,11 +39,35 @@ public class RoomController {
 	private RoomService roomService;
 
 	@GetMapping
-	public String getRooms(Model model) {
+	public String getRooms(Model model, @ModelAttribute RoomDto roomDto) {
 		List<RoomDto> roomDtos = roomService.getAllRooms();
 		model.addAttribute("roomDtos", roomDtos);
 		return "room";
 	}
 	
+	@PostMapping
+	// @Valid 進行驗證
+	// BindingResult 驗證結果
+	public String addRoom(@Valid RoomDto roomDto, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("roomDtos", roomService.getAllRooms());
+			return "room";
+		}
+		
+		roomService.addRoom(roomDto);
+		return "redirect:/rooms"; // 重導到 /rooms 頁面
+	}
+	
+	@GetMapping("/delete/{roomId}")
+	public String deleteRoom(@PathVariable("roomId") Integer roomId) {
+		roomService.deleteRoom(roomId);
+		return "redirect:/rooms"; // 重導到 /rooms 頁面
+	}
+	
+	@ExceptionHandler({RoomException.class})
+	public String handleRoomException(RoomException e, Model model) {
+		model.addAttribute("message", e.getMessage());
+		return "error";
+	}
 	
 }
