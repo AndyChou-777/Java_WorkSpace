58392797dd61bcae8be4dbda4188de83436ed1cf
NTPU <NTPU@503-11>
2024-11-19 12:13:04 +0800

    javaweb

--- springboot-tx/src/main/java/com/example/tx/repository/WalletRepository.java
+++ springboot-tx/src/main/java/com/example/tx/repository/WalletRepository.java
@@ -1,8 +1,10 @@
 package com.example.tx.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.tx.entity.Wallet;
 
@@ -13,6 +15,8 @@ public interface WalletRepository extends JpaRepository<Wallet, String> {
 	Integer getWalletBalance(String username);
 	
 	// 更新餘額
+	@Transactional
+	@Modifying
 	@Query(value = "update wallet set balance = balance - :bookPrice where username = :username", nativeQuery = true)
 	void updateWalletBalance(Integer bookPrice, String username);
 }
