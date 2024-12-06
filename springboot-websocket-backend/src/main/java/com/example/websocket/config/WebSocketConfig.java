384835d56a26c4bf83cbb0cf9d4b6a3d3a30a260
NTPU <NTPU@503-11>
2024-12-06 15:50:44 +0800

    JavaWeb

--- springboot-websocket-backend/src/main/java/com/example/websocket/config/WebSocketConfig.java
+++ springboot-websocket-backend/src/main/java/com/example/websocket/config/WebSocketConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 import com.example.websocket.channel.ChatChannel;
+import com.example.websocket.channel.StockWebSocketService;
+import com.example.websocket.channel.TimeWebSocketService;
+import com.example.websocket.channel.WebSocketService;
 
 @Configuration
 public class WebSocketConfig {
@@ -13,7 +16,8 @@ public class WebSocketConfig {
 	public ServerEndpointExporter serverEndpointExporter() {
 		ServerEndpointExporter exporter = new ServerEndpointExporter();
 		// 註冊 WebSocket 端點
-		exporter.setAnnotatedEndpointClasses(ChatChannel.class);
+		// 門號: /channel/chat, /time-service
+		exporter.setAnnotatedEndpointClasses(ChatChannel.class, TimeWebSocketService.class, StockWebSocketService.class, WebSocketService.class);
 		
 		return exporter;
 	}
--- springboot-websocket-backend/src/test/java/com/example/websocket/SpringbootWebsocketBackendApplicationTests.java
+++ springboot-websocket-backend/src/test/java/com/example/websocket/SpringbootWebsocketBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.proxy.websocket;
+package com.example.websocket;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
